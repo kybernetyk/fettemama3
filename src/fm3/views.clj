0a0023cf30b33d0ae477f24313c431570aa9f6c6
Leon Szpilewski <leon.szpilewski@gmail.com>
2012-12-16 00:56:19 +0100

    bottleneck fixd

--- src/fm3/views.clj
+++ src/fm3/views.clj
@@ -3,6 +3,7 @@
   (:require fm3.blog)
   (:require [clj-time.core :as time])
   (:require [clj-time.format :as timeformat])
+  (:require [clj-time.coerce :as coerce])
   (:require [clojure.java.io :as io])
   (:require [clojure.string :as string])
   (:use [ring.util.response :only [redirect]]))
@@ -23,33 +24,47 @@
 ;; ----------------------------------------- POST RENDERING -------------------------
 ; ----------- post renderer -----------------
 ; convert a post timestamp to human readable format
-(defn make-date [timestamp]
-  (def formatter (timeformat/formatter "yyyy-MM-dd HH:mm:ss.S"))
-  (def date (timeformat/parse formatter timestamp))
+
+(def formatter 
+  (timeformat/formatter "yyyy-MM-dd HH:mm:ss.S"))
+
+;turns a database timestamp into a human readable timestamp
+(defn make-date-from-timestamp [timestamp]
+  (def date (timeformat/parse formatter (str timestamp)))
   (.toString date "EE MMM dd yyyy"))
 
-; get's a human readable date string from a post parsing its :timestamp 
-(defn date-for-post [post]
-  (make-date (str (:timestamp post))))
+(def ux-formatter 
+  (timeformat/formatter "EE MMM dd yyyy"))
 
-; makes a list of distinct dates from 'posts'
-(defn make-date-list [posts]
-  (distinct (map date-for-post posts)))
+;turns a human readable timestamp into a unix timestamp (ish)
+(defn unix-ts-for-date [date]
+  (def uxts (timeformat/parse ux-formatter (str date)))
+  (coerce/to-long uxts))
 
-; makes a map{list} of posts belonging to 'date'
-(defn posts-for-date [posts date]
-  (def the-posts 
-    (filter (fn [post] (= (date-for-post post) date)) 
-      posts))
-  {:date date :posts the-posts})
+;make a clostache compatible post from database post
+(defn make-post [raw-post-data]
+  {:date (make-date-from-timestamp (:timestamp raw-post-data))
+   :content (:content raw-post-data)
+   :id (:id raw-post-data)})
 
-; sort posts by date and put them into a clostache compatible map
-; this is very ugly as I'm a functional lamer :]
-; output is:
+; make a day entry for the post list
+; input:
+; [$date [{post}, {post}, ...]]
+(defn make-day [post-sublist]
+  (def date (first post-sublist))
+  {:date date 
+   :posts (second post-sublist) 
+   :unix-ts (unix-ts-for-date date)})
+
+; made a list of post grouped by days
+; output:
 ; {:days [{:date distinct-day :posts [list-of-posts]}]}
 (defn make-post-list [posts]
-  (def dates (make-date-list posts))
-  {:days (map (fn [date] (posts-for-date posts date)) dates)})
+  (def post-list (map make-post posts))
+  (def grouped-list (group-by :date post-list)) 
+  (def day-list (map make-day grouped-list))
+  {:days (reverse (sort-by :unix-ts day-list))})
+
 
 (defn render-posts [posts]
   (render-page "templates/index.mustache" (make-post-list posts)))
