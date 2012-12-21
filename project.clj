9d5a688b60c2c3cec21809a48797f13649f078f2
Leon Szpilewski <leon.szpilewski@gmail.com>
2012-12-21 23:40:09 +0100

    updated to lein2

--- project.clj
+++ project.clj
@@ -2,11 +2,13 @@
   :description "fettemama-3 blogging system"
   :url "http://example.com/FIXME"
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [compojure "1.1.3"]
-                 [korma "0.3.0-beta7"]
-                 [mysql/mysql-connector-java "5.1.6"]
-                 [de.ubercode.clostache/clostache "1.3.1"]
-                 [clj-time "0.4.4"]]
+                 [compojure "1.1.3" :exclusions [org.clojure/clojure]]
+                 [korma "0.3.0-beta7" :exclusions [org.clojure/clojure]]
+                 [mysql/mysql-connector-java "5.1.6" :exclusions [org.clojure/clojure]]
+                 [de.ubercode.clostache/clostache "1.3.1" :exclusions [org.clojure/clojure]]
+                 [clj-time "0.4.4" :exclusions [org.clojure/clojure]]
+                 [org.clojure/clojure "1.4.0"]]
+
   :plugins [[lein-ring "0.7.5"]]
-  :ring {:handler fm3.routes/app}
-  :dev-dependencies [[ring-mock "0.1.3"]])
+  :ring {:handler fm3.routes/app})
+;  :dev-dependencies [[ring-mock "0.1.3"]])
