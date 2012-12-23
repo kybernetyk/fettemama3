13890682d7d940c5d79bce313f64b768f87d0ace
Leon Szpilewski <leon.szpilewski@gmail.com>
2012-12-23 23:05:07 +0100

    remove milliseconds from dates

--- src/fm3/views/post.clj
+++ src/fm3/views/post.clj
@@ -1,6 +1,7 @@
 (ns fm3.views.post
   (:require [fm3.data.posts :as posts])
   (:require [fm3.data.comments :as comments])
+  (:require [clj-time.format :as timeformat])
   (:require [fm3.data.users :as users])
   (:require [fm3.views.common :as common]))
 
@@ -20,9 +21,18 @@
 (defn get-verb []
   (verbs (rand-int (count verbs))))
 
+(def formatter 
+  (timeformat/formatter "yyyy-MM-dd HH:mm:ss.S"))
+
+;turns a database timestamp into a human readable timestamp
+(defn make-timestamp [timestamp]
+  (let [date (timeformat/parse formatter (str timestamp))]
+    (.toString date "EE MMM dd yyyy HH:mm:ss")))
+
 (defn prepare-comment [comment]
   (assoc comment :author (users/name-by-url (:authorurl comment)) 
-                 :verb (get-verb)))
+                 :verb (get-verb)
+                 :timestamp (make-timestamp (:timestamp comment))))
 
 (defn append-comments [post]
   (let [comments (comments/comments-by-parent-url (posts/url-for-post-id (:id post)))]
@@ -32,6 +42,7 @@
 
 (defn render-post [post-id]
 	(let [post (posts/post-by-id post-id)
+       post (assoc post :timestamp (make-timestamp (:timestamp post)))
         post-with-comments (append-comments post)]
    (if (not post)
      (common/render-404 {})
