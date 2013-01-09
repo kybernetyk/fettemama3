90d56bb389de52ecad03f346fad5c301b942d178
Leon Szpilewski <leon.szpilewski@gmail.com>
2013-01-09 20:11:55 +0100

    catch spam comments before they're written to the db

--- src/fm3/views/comment.clj
+++ src/fm3/views/comment.clj
@@ -5,8 +5,17 @@
   (:require [fm3.views.common :as common])
   (:use [ring.util.response :only [redirect]]))
 
+(defn string-contains? [^String big ^String little]
+      (not (neg? (.indexOf big little))))
+
+(defn process-new-comment [author-name post-id content]
+  (let [author-url (users/url-by-name author-name)
+        post-url (posts/url-for-post-id post-id)]
+    (comments/create-comment author-url post-url content)
+    (redirect post-url)))
+
 (defn post-new-comment [author-name post-id content]
-	(let [author-url (users/url-by-name author-name)
-		post-url (posts/url-for-post-id post-id)]
-	(comments/create-comment author-url post-url content)
-	(redirect post-url)))
+  (if (string-contains? content "href=")
+    (redirect "http://127.0.0.1")
+    (process-new-comment author-name post-id content)))
+
